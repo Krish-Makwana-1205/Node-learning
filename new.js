3f170c47116135eea909e22a22c1161f84b23704
Krish-Makwana-1205 <krish.rajput1205@gmail.com>
2024-06-08 18:01:36 +0530

    Creating a server with log and adding url package

--- new.js
+++ new.js
@@ -1,8 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
 const firstserver = http.createServer((req, res) => {
-    console.log(req);
-    res.end("Server on lets go");
+    const x = 'New user\n';
+    fs.appendFile('sample.txt', x, (err, data)=>{
+        res.end("Server on lets go");
+    });
 });
 
 firstserver.listen(8000, ()=> console.log("We are online"));
