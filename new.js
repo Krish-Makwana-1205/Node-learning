545da989455c13811f884a92ad0dc1fb9f43e6c8
Krish-Makwana-1205 <krish.rajput1205@gmail.com>
2024-06-08 19:28:35 +0530

    Interacting with url

--- new.js
+++ new.js
@@ -1,11 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const firstserver = http.createServer((req, res) => {
-    const x = 'New user\n';
-    fs.appendFile('sample.txt', x, (err, data)=>{
-        res.end("Server on lets go");
-    });
+    const myurl = url.parse(req.url, true);
+    if(myurl.pathname == '/favicon.ico'){
+        return res.end();
+    }
+    if(myurl.query.name){
+        let username = myurl.query.name
+        res.end("Hi " + username);
+    }
+    else{
+        res.end("Hola");
+    }
+    console.log(myurl);
 });
 
 firstserver.listen(8000, ()=> console.log("We are online"));
