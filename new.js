2733d4243e0727ad6734038515885fe3f9af941e
Krish-Makwana-1205 <krish.rajput1205@gmail.com>
2024-06-09 10:35:21 +0530

    express is on

--- new.js
+++ new.js
@@ -1,20 +1,15 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
 
-const firstserver = http.createServer((req, res) => {
-    const myurl = url.parse(req.url, true);
-    if(myurl.pathname == '/favicon.ico'){
-        return res.end();
-    }
-    if(myurl.query.name){
-        let username = myurl.query.name
-        res.end("Hi " + username);
-    }
-    else{
-        res.end("Hola");
-    }
-    console.log(myurl);
+let app = express();
+
+app.get('/', (req, res)=>{
+    return  res.send("Hello home here");
+});
+app.get('/about', (req, res)=>{
+    return res.send("Hello from about page, " + req.query.name + req.query.age);
 });
 
-firstserver.listen(8000, ()=> console.log("We are online"));
+app.listen(8000, (req, res)=> {
+    console.log("Server is up and running");
+})
